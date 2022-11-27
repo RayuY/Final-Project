625c42b0455f4124f87fcc4480037c006d6ba08d
Matt <Mwilson82@hotmail.com>
2022-11-26 19:49:30 -0800

    fix owners table naming

--- server/db/seeds/03_owners.sql
+++ server/db/seeds/03_owners.sql
@@ -1,8 +1,8 @@
 -- seeds/03_owners.sql
 -- owners seeds
-INSERT INTO owners (owner_name, owner_email, owner_password, owner_phone) VALUES ('Aldwin Bautista', 'aldwin@bautista.com', '12345', '6041234567');
-INSERT INTO owners (owner_name, owner_email, owner_password, owner_phone) VALUES ('Matt Wilson', 'matt@wilson.com', '54321', '6041234567');
-INSERT INTO owners (owner_name, owner_email, owner_password, owner_phone) VALUES ('Ray Yu', 'ray@yu.com', '24680', '6041234567');
+INSERT INTO owners (name, email, passphone) VALUES ('Aldwin Bautista', 'aldwin@bautista.com', '12345', '6041234567');
+INSERT INTO owners (name, email, password, phone) VALUES ('Matt Wilson', 'matt@wilson.com', '54321', '6041234567');
+INSERT INTO owners (name, email, password, phone) VALUES ('Ray Yu', 'ray@yu.com', '24680', '6041234567');
 
 
 
