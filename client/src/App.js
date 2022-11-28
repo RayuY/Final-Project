b9b8f7f47af36770c46024527cdcfbf41a1f6f2f
Ray Yu <kaito-akira@hotmail.com>
2022-11-28 10:42:26 -0800

    refactor and clean up

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 import './App.css';
-import Navbar from './components/Navbar/Navbar'
+import { Navbar } from './components';
+import { Footer } from './container';
 
 function App() {
 
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 }
