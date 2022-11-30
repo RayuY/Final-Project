b629d065aa73dcfc7f35751d4de29f174c7c886e
Matt <Mwilson82@hotmail.com>
2022-11-30 15:55:28 -0800

    fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import { Navbar, ScrollButton, Owner, Homepage, User, AboutUs } from "./components";
+import { Navbar, ScrollButton, Owner, Homepage, User, AboutUs, Login, Register } from "./components";
 import { Footer } from "./container";
 
 import { BrowserRouter as Router, Route, Routes, Link, Redirect, } from "react-router-dom";
 
-import { UserContext } from './UserContext'
+import { UserContext } from './UserContext';
 import { useMemo, useState } from "react";
 import Restaurant from "./components/Restaurant/Restaurant";
 import OwnerReservation from "./components/OwnerReservation/OwnerReservation";
@@ -12,7 +12,7 @@ import OwnerReservation from "./components/OwnerReservation/OwnerReservation";
 function App() {
 
   const [user, setUser] = useState('Guest');
-  const role = useMemo(() => ({ user, setUser }), [user, setUser])
+  const role = useMemo(() => ({ user, setUser }), [user, setUser]);
 
   return (
     <div>
@@ -32,7 +32,7 @@ function App() {
             <Route exact path="/about" element={<AboutUs />} />
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/register" element={<Register />} />
-            <Route exact path="/reservations/:id" element={<OwnerReservation />}/>
+            <Route exact path="/reservations/:id" element={<OwnerReservation />} />
           </Routes>
 
           <Footer />
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,11 +2,13 @@ import SubHeading from './SubHeading/SubHeading';
 import Newsletter from './Footer/Newsletter';
 import FooterOverlay from './Footer/FooterOverlay';
 import Navbar from './Navbar/Navbar';
-import ScrollButton from './ScrollButton/ScrollButton'
+import ScrollButton from './ScrollButton/ScrollButton';
 import Homepage from './Homepage/Homepage';
 import AboutUs from './AboutUs/AboutUs';
 import Owner from './Owner/Owner';
-import User from './User/User'
+import User from './User/User';
+import Login from './Login/Login';
+import Register from './Register/Register';
 
 
 export {
@@ -19,4 +21,6 @@ export {
   Newsletter,
   FooterOverlay,
   Navbar,
+  Login,
+  Register
 };
