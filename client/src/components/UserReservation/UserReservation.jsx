834b96fa7497e1d107c5d55b305e0bc4fd616fe2
Matt <Mwilson82@hotmail.com>
2022-11-30 23:14:44 -0800

    css styling for confirm-reservation page

--- client/src/components/UserReservation/UserReservation.jsx
+++ client/src/components/UserReservation/UserReservation.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import './UserReservation.css';
+import { images } from "../../constants";
 
 
 function UserReservation(props) {
@@ -30,9 +32,33 @@ function UserReservation(props) {
 
   return (
 
-    <div>
-      {restaurant.title}
-      {restaurant.address}
+    <div className="res_div">
+      <div className="res_panel">
+        <h5>Your Reservation has been confirmed</h5>
+        <div className="res_div_spoon">
+          <img
+            src={images.spoon}
+            className="spoon__img"
+            style={{ marginTop: 15, transform: "rotate(180deg)" }}
+          />
+          <img
+            src={images.spoon}
+            className="spoon__img"
+            style={{ marginTop: 15 }}
+          />
+        </div>
+        <h4>{restaurant.title}</h4>
+        <img className="restaurantimg" src={restaurant.img} />
+        <h6>{restaurant.address}</h6>
+      </div>
+      <div className="res_bottom_links">
+        <button className="custom__button">
+          Home Page
+        </button>
+        <button className="custom__button">
+          Cancel Reservation
+        </button>
+      </div>
     </div>
 
 
