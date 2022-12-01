f7e3dadd7ba5fa6de9dcdf565299a39863a509e9
aldwinb <aldwinb23@gmail.com>
2022-12-01 12:41:01 -0800

    resolve merge conflicts

--- server/app.js
+++ server/app.js
@@ -9,8 +9,6 @@ const usersRouter = require('./routes/users');
 const ownersRouter = require('./routes/owners');
 const restaurantsRouter = require('./routes/restaurants');
 const reservationsRouter = require('./routes/reservations');
-const ownersRouter = require('./routes/owners');
-const usersRouter = require('./routes/users')
 
 const app = express();
 
@@ -30,9 +28,5 @@ app.use('/restaurants', restaurantsRouter);
 app.use('/restaurants/:id', restaurantsRouter);
 app.use('/reservations', reservationsRouter);
 app.use('/reservations/:id', reservationsRouter);
-app.use('/owners', ownersRouter);
-app.use('/owners/:id', ownersRouter);
-app.use('/users', usersRouter);
-app.use('/users/:id', usersRouter);
 
 module.exports = app;
