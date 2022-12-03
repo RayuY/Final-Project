f33cb61a30f664747cd4a80d4d1a95115892435c
Matt <Mwilson82@hotmail.com>
2022-12-02 23:28:02 -0800

    implement css styling for reservation confirmation

--- client/src/components/UserReservation/UserReservation.jsx
+++ client/src/components/UserReservation/UserReservation.jsx
@@ -25,13 +25,13 @@ function UserReservation({ userObj }) {
         setRestaurantInfo(all[1].data.restaurants);
         setUserInfo(all[2].data);
       })
-      .then(()=> {console.log("test")})
+      .then(() => { console.log("test"); })
       .catch((e) => console.error(`Error: ${e}`));
   }, []);
 
   if (reservationInfo.length === 0) {
     return null;
-  } 
+  }
   if (restaurantInfo.length === 0) {
     return null;
   }
@@ -43,22 +43,23 @@ function UserReservation({ userObj }) {
 
   const userId = reservation1.user_id;
   const restaurantId = reservation1.restaurant_id;
-  
+
   const name1 = userInfo.users.find((i) => i.id === userId);
 
-  const userName = name1.name
+  const userName = name1.name;
 
   const rest1 = restaurantInfo.find((i) => i.id === restaurantId);
 
-  const restaurantName = rest1.title
-  const restaurantImg = rest1.img
-  const restaurantAddress = rest1.address
+  const restaurantName = rest1.title;
+  const restaurantImg = rest1.img;
+  const restaurantAddress = rest1.address;
+  const restaurantPhone = rest1.phone;
 
   return (
     <div className="res_div">
       <div className="res_panel">
-        <h5>Your Reservation has been confirmed</h5>
-        <h2>{userName}</h2>
+        <h2>Your Reservation has been confirmed!</h2>
+        <h2 className="res_user">{userName}</h2>
         <div className="res_div_spoon">
           <img
             src={images.spoon}
@@ -71,9 +72,10 @@ function UserReservation({ userObj }) {
             style={{ marginTop: 15 }}
           />
         </div>
-        <h4>{restaurantName}</h4>
         <img className="restaurantimg" src={restaurantImg} />
-        <h6>{restaurantAddress}</h6>
+        <h1 className="res_name">{restaurantName}</h1>
+        <h3>{restaurantAddress}</h3>
+        <h3 className="res_phone">250-669-6969</h3>
       </div>
       <div className="res_bottom_links">
         <button className="custom__button">Home Page</button>
