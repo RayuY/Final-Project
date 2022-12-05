3ebbf2f80317cf057d878431a063645e3463b31e
Ray Yu <kaito-akira@hotmail.com>
2022-12-04 21:29:07 -0800

    db change

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,8 +1,8 @@
 -- seeds/01_users.sql
 -- users seeds
 INSERT INTO users (name, email, password, phone, is_owner) VALUES ('Matt Wilson', 'matt@wilson.com', '54321', '6041234567', false);
-INSERT INTO users (name, email, password, phone, is_owner) VALUES ('Ray Yu', 'ray@yu.com', '24680', '5041234567', false);
-INSERT INTO users (name, email, password, phone, is_owner) VALUES ('Michael Jordan', 'michale@jordan.com', '24680', '5041234567', false);
+INSERT INTO users (name, email, password, phone, is_owner) VALUES ('Matt', 'ray@yu.com', '24680', '5041234567', false);
+INSERT INTO users (name, email, password, phone, is_owner) VALUES ('Aldwin', 'michale@jordan.com', '24680', '5041234567', false);
 INSERT INTO users (name, email, password, phone, is_owner) VALUES ('Bugs Bunny', 'bugs@bunny.com', '24680', '5041234567', false);
 
 
