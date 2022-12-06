4d28267ff7cad0074836d045b0f0afba09ecab3c
aldwinb <aldwinb23@gmail.com>
2022-12-06 15:58:40 -0800

    delete console logs

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,16 +6,13 @@ const users = require('../db/queries/users');
 /* GET customers listing. */
 router.get('/:id', (req, res) => {
   const id = req.params.id;
-  console.log("test test", id);
   users.getUserById(id).then(data => {
-    console.log(data);
     res.json({ users: data });
   });
 });
 
 router.get('/', (req, res) => {
   users.getAllUsers().then(data => {
-    console.log(data);
     res.json({ users: data });
   });
 });
