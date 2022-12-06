3223f469868774eb72f80e31936a612410847589
aldwinb <aldwinb23@gmail.com>
2022-12-06 11:21:59 -0800

    clean up logs from routes

--- server/twilio.js
+++ server/twilio.js
@@ -1,17 +1,11 @@
 
 require('dotenv').config();
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Find your Account SID and Auth Token at twilio.com/console
-// and set the environment variables. See http://twil.io/secure
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 const twilioSender = process.env.TWILIO_SENDER;
 const twilioReceiver = process.env.TWILIO_RECEIVER
-
-console.log("accSid:", accountSid);
-console.log("twiSen:", twilioSender);
-console.log("twiRec:", twilioReceiver);
 
 client.messages
   .create({
