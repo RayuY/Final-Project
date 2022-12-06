88e31fc1684ed9976a96705013d1b3c9b9894b2c
Matt <Mwilson82@hotmail.com>
2022-12-06 15:06:43 -0800

    twilio changes

--- server/twilio.js
+++ server/twilio.js
@@ -7,17 +7,14 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 const twilioSender = process.env.TWILIO_SENDER;
-const twilioReceiver = process.env.TWILIO_RECEIVER
+const twilioReceiver = process.env.TWILIO_RECEIVER;
 
-console.log("accSid:", accountSid);
-console.log("twiSen:", twilioSender);
-console.log("twiRec:", twilioReceiver);
 
 client.messages
   .create({
-     body: 'Your reservation has been confirmed. Thanks!',
-     from: twilioSender,
-     to: twilioReceiver
-   })
+    body: 'Your reservation has been confirmed. Thanks!',
+    from: twilioSender,
+    to: twilioReceiver
+  })
   .then(message => console.log(message.sid))
   .catch((err) => console.log("Error:", err));
